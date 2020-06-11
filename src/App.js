25f08ee9bba690fa1969cc73217e287a8943ba3c
Muhammad Masood <muhammadmasood673@gmail.com>
2020-06-11 22:01:41 +0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React, { Component }from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Navbar from './FirstComponent';
+import { Button } from 'reactstrap';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {  }
-  }
-  render() { 
-    return ( 
-      <div >
-        <Navbar/>
+
+function App() {
+  let [count, setCount] = useState(0);
+  let [isMorning, setMorning] = useState(true);
+  return (
+    <div className="container box mt-2">
+      <div className="row justify-content-center  mt-5">
+        <h3><i>Good {isMorning ? 'Morning' : 'Night'}</i></h3>
       </div>
-     );
-  }
+      <div className="row justify-content-center  mt-2">
+        <Button outline onClick={() => setMorning(!isMorning)} color="success">Day Time</Button>
+      </div>
+      <div className="row justify-content-center  mt-5 ">
+        <h3>{isMorning ? <i class="fa fa-lightbulb-o onBulb mr-5" aria-hidden="true"></i> : <i class="fa fa-lightbulb-o ofBulb mr-5" aria-hidden="true"></i>}</h3>
+      </div>
+      <div className="row justify-content-center  mt-5">
+        <h3> Counter: {count}</h3>
+      </div>
+      <div className="row justify-content-center  mt-2">
+        <Button outline onClick={() => setCount(count + 1)} color="success">Update Counter</Button>
+      </div>
+    </div>
+  );
 }
- 
+
+
 export default App;

fb2984fa1b354064b7c05cb2f659aacd999f0ab1
Muhammad Masood <muhammadmasood673@gmail.com>
2020-06-15 11:59:29 +0500

    Updated Application

--- src/Component/AddTransaction.js
+++ src/Component/AddTransaction.js
@@ -10,10 +10,10 @@ const AddTransaction = () => {
                 <Form >
                     <FormGroup>
                         <Label id="label" for="exampleEmail">Text</Label>
-                        <Input type="text"  name="text" id="exampleEmail" placeholder="Enter Text..." size="34" />
+                        <Input type="text"  name="text" id="exampleEmail" placeholder="Enter Text..." size="30" />
                         <FormText>Amount</FormText>
                         <FormText>[Negative - Expense, Positive - Income]</FormText>
-                        <Input type="text"  name="text" id="exampleEmail" placeholder="0" className="mt-3" size="34" />
+                        <Input type="text"  name="text" id="exampleEmail" placeholder="0" className="mt-3" size="30" />
                         <Button color="primary" size="lg"  className="mt-3" block>Block level button</Button>
                     </FormGroup>
                 </Form>
